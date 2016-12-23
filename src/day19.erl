453898a3b220621e43eaf04e33dd8ecc1706f2df
Hakan Nilsson <haakan@gmail.com>
2016-12-24 00:18:38 +0100

    Day 19 Part 2

--- src/day19.erl
+++ src/day19.erl
@@ -12,24 +12,37 @@ solve(NumElves) ->
     solve(lists:seq(1, NumElves), []).
 
 solve2(NumElves) ->
-    solve2(lists:seq(1, NumElves), []).
+    X = find_pow(1, NumElves),
+    case 2*X < NumElves of
+        false -> NumElves-X;
+        true  -> X + 2*(NumElves-(2*X))
+    end.
+
+find_pow(Pow, NumElves) ->
+    case math:pow(3, Pow) >= NumElves of
+        true  -> trunc(math:pow(3, Pow-1));
+        false -> find_pow(Pow+1, NumElves)
+    end.
 
 solve([], [Elf])       -> Elf;
 solve([N,_|Rest], Acc) -> solve(Rest, [N|Acc]);
 solve(E, Acc)          -> solve(E ++ lists:reverse(Acc), []).
 
+solve2_sim(NumElves) ->
+    solve2_sim(lists:seq(1, NumElves), []).
 
-solve2([], [Elf])     -> Elf;
-solve2([Elf], Acc)    -> solve2([Elf|lists:reverse(Acc)], []);
-solve2([], Acc)       -> solve2(lists:reverse(Acc), []);
-solve2([N|Rest], Acc) ->
-    %% This works, but it super slow
-    Elves = Rest ++ lists:reverse(Acc),
-    Len = length(Elves),
+solve2_sim([], [Elf])     -> Elf;
+solve2_sim([Elf], [])     -> Elf;
+solve2_sim([Elf], Acc)    -> solve2_sim([Elf|lists:reverse(Acc)], []);
+solve2_sim([], Acc)       -> solve2_sim(lists:reverse(Acc), []);
+solve2_sim([N|Rest], Acc) ->
+    %% This works, but it super slow, used it to figure out the pattern
+    Elves  = Rest ++ lists:reverse(Acc),
+    Len    = length(Elves),
     Remove = Len div 2 + (Len rem 2),
     Nth    = lists:nth(Remove, Elves),
     io:format("~p removes ~p [~p] (len: ~p)\n", [N, Nth, Remove, Len]),
-    solve2(lists:delete(Nth, Rest), [N|lists:delete(Nth, Acc)]).
+    solve2_sim(lists:delete(Nth, Rest), [N|lists:delete(Nth, Acc)]).
 
 %%%_* Tests ============================================================
 -include_lib("eunit/include/eunit.hrl").
@@ -37,5 +50,8 @@ solve2([N|Rest], Acc) ->
 day19_test_() ->
     [ ?_assertEqual(3, solve(5))
     , ?_assertEqual(1834471, solve_part1())
-    , ?_assertEqual(2, solve2(5))
-    ].
+    , ?_assertEqual(1420064, solve_part2())
+    , ?_assertEqual(9, solve2(90))
+    , ?_assertEqual(241, solve2(242))
+    ] ++ [?_assertEqual( solve2_sim(N)
+                       , solve2(N)) || N <- lists:seq(2, 100)].
